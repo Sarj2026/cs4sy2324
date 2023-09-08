bb8f94726216ef85026b4c31a0e9fba3d71af4d6
Sarj2026 <143585791+Sarj2026@users.noreply.github.com>
2023-09-08 16:04:14 +0800

    Update Cs4trutharnesto.java

--- src/cs4trutharnesto/Cs4trutharnesto.java
+++ src/cs4trutharnesto/Cs4trutharnesto.java
@@ -16,8 +16,22 @@ public class Cs4trutharnesto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String greeting =("Ni Hao! Magandang Umaga ");
-        System.out.println( greeting + "Hello World");
+        String volleyballplayer1 =("Brent Cristobal ");
+        String position1 =("Wing Spiker ");
+        String sex1 =("Male ");
+        System.out.println("Volleyball Player 1 \nName: " + volleyballplayer1 + "\nVolleyball Position: " + position1 + "\nSex: " + sex1);
+        String volleyballplayer2 =("DC Orongan ");
+        String position2 =("Libero ");
+        String sex2 =("Female ");
+        System.out.println("\n\nVolleyball Player 2 \nName: " + volleyballplayer2 + "\nVolleyball Position: " + position2 + "\nSex: " + sex2);
+        String volleyballplayer3 =("Miguel Cayetano ");
+        String position3 =("Wing Spiker ");
+        String sex3 =("Male ");
+        System.out.println("\n\nVolleyball Player 3 \nName: " + volleyballplayer3 + "\nVolleyball Position: " + position3 + "\nSex: " + sex3);
+
+        System.out.println("\n\nIt is "+sex1==sex3+" that "+volleyplayer1+" and "+volleyplayer3+" are the same gender");
+        System.out.println("\nIt is "+position1==position3+" that "+volleyplayer1+" and "+volleyplayer3+" play the same position");
+        System.out.println("\nIt is "+sex1==sex2+" that "+volleyplayer1+" and "+volleyplayer2+" are the same gender");
     }
     
 }
